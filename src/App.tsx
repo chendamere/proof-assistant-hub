425160ea3d2c1ecf6c300f578f9fb9a92fe5429d
Hankai Chen <87444158+chendamere@users.noreply.github.com>
2026-02-10 19:28:10 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { ThemeProvider } from "next-themes";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PanelProvider } from "@/contexts/PanelContext";
 import Index from "./pages/Index";
-import Verifier from "./pages/Verifier";
 import ProofStep from "./pages/ProofStep";
 import ProofSteps from "./pages/ProofSteps";
 import SubstitutionDAGDemo from "./pages/SubstitutionDAGDemo";
@@ -28,12 +27,8 @@ const App = () => (
           <Routes>
             {/* Auth route removed – all pages are publicly accessible */}
             <Route path="/" element={<Index />} />
-            <Route path="/verifier" element={<Verifier />} />
-            <Route path="/proof-step" element={<ProofStep />} />
             <Route path="/proof-steps" element={<ProofSteps />} />
             <Route path="/substitution-dag" element={<SubstitutionDAGDemo />} />
-            <Route path="/substitution-example" element={<SubstitutionExample />} />
-            <Route path="/glossary" element={<Glossary />} />
             <Route path="/bibliography" element={<Bibliography />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
